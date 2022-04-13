c9c091131bedbb4e6e9ec105e1f9e558d1c8c3c2
Damian Rzeszot <damian.rzeszot@gmail.com>
2022-04-13 13:19:40 +0200

    Update package name

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftGenPlugin",
+    name: "SwiftGenPackage",
     products: [
         .plugin(
             name: "SwiftGenPlugin",
